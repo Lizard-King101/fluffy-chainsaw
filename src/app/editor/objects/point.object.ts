e5cd7edb880199b67226b8415245dc0e260ae645
unknown <nicholas.labrecque01@gmail.com>
2021-08-28 21:15:59 -0400

    point update

--- src/app/editor/objects/point.object.ts
+++ src/app/editor/objects/point.object.ts
@@ -1,9 +1,11 @@
 
 export class Point {
+    id: string;
     x: number;
     y: number;
 
     constructor(x: number, y: number) {
+        this.id = Math.random().toString(36).substr(2, 9);
         this.x = x;
         this.y = y;    
     }
